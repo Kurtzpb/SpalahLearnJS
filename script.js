1c9f0f1898f7765d9fc4124eb599cdeb8e2cc570
Aleksey Kuzmenko <kurtzpb@gmail.com>
2017-01-24 13:47:27 +0200

    Home task

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-function getSum() {
-	var index = 1;
-	var sum = "";
-	console.log("Arguments length = " + arguments.length + "\n*********");
-	if (arguments.length === 0) {
-		console.log("Empty");
-	}
-	else {
-		for (var i = 0; i < arguments.length; i++) {
-			console.log("Argument " + index++ + " = " + arguments[i] + "\n*********");
-			sum += arguments[i] + ", ";
-		}
-	}
-	return (console.log("Sum is " + sum));
-}
+// function getSum() {
+// 	var index = 1;
+// 	var sum = "";
 
-getSum(5, 8, "Hello", 7);
+// 	console.log("Arguments length = " + arguments.length + "\n*********");
+// 	if (arguments.length === 0) {
+// 		console.log("Empty");
+// 	}
+// 	else {
+// 		for (var i = 0; i < arguments.length; i++) {
+// 			console.log("Argument " + index++ + " = " + arguments[i] + "\n*********");
+// 			sum += arguments[i] + ", ";
+// 		}
+// 	}
+// 	return (console.log("Sum is " + sum));
+// }
 
-(function () { 
-	console.log("------------------" + "\nHi!");
-}());
+// getSum(5, 8, "Hello", 7);
+
+// (function () { 
+// 	console.log("//////////////" + "\nHi!");
+// }());
+
+var random = Math.random()
+
+console.log(random);
+console.log(random <= 0.33)
